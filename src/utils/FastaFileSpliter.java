cc34d24b1bd8e623b7218b3051846641a45c25a8
jiserte <jiserte@ligbcm01>
2011-05-27 12:41:48 -0300

    bug corrected

--- src/utils/FastaFileSpliter.java
+++ src/utils/FastaFileSpliter.java
@@ -17,8 +17,11 @@ public class FastaFileSpliter {
 		FastaMultipleReader fmr = new FastaMultipleReader();
 		FastaWriter fw = new FastaWriter();
 
-		String infile = JOptionPane.showInputDialog(null, "Input File: ",  "Enter Input File", 1);
-		String outdir = JOptionPane.showInputDialog(null, "Output Dir: ",  "Enter Output Dir", 1);
+		//String infile = JOptionPane.showInputDialog(null, "Input File: ",  "Enter Input File", 1);
+		//String outdir = JOptionPane.showInputDialog(null, "Output Dir: ",  "Enter Output Dir", 1);
+		
+		String infile = "C:\\JAvier\\DropBox\\My Dropbox\\Investigacion\\Sandra\\Filogenia SLEV - Mayo 2011\\Datos de Partida\\SLEV ORF nucleotido.fas";
+		String outdir = "C:\\JAvier\\DropBox\\My Dropbox\\Investigacion\\Sandra\\Filogenia SLEV - Mayo 2011\\Datos de Partida\\separados";		
 		
 		File file = new File(infile);
 		List<Pair<String, String>> fas = null;
@@ -28,11 +31,17 @@ public class FastaFileSpliter {
 			e.printStackTrace();
 		}
 		
+		System.out.println(fas);
+		
 		for (Pair<String, String> pair : fas) {
 			String s = pair.getSecond();
 			String d = pair.getFirst();
+
+			String fname = outdir + "\\" + d.substring(0,Math.min(30, d.length()))+ ".fas"; 
+			fname =fname.replaceFirst("/", "_");
+			fname =fname.replaceFirst("/", "_");
+			fw.writeFile(fname, s, d);
 			
-			fw.writeFile(outdir + "\\" + d.substring(0,30) , s, d);
 		}
 	}
 }
