8bf8bbf8946375aee40a5ad4fa9807399262ca33
Javier Iserte <jiserte@unq.edu.ar>
2012-02-28 15:57:53 -0300

    Added methods to public interface to make more easy the use.

--- src/fastaIO/FastaWriter.java
+++ src/fastaIO/FastaWriter.java
@@ -1,20 +1,59 @@
 package fastaIO;
 
-import java.io.IOException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.Vector;
+
 
 public class FastaWriter {
 
-	public void writeFile(String output, String sequence, String description) {
-		PrintWriter pw = null;
-		try {
-			pw = new PrintWriter(output);
-		} catch (IOException e) {
-			System.out.println("An error ocurred while writing the file");
-		}  
-		pw.println(">" + description);
-		pw.println(sequence);
-		pw.close();
+	///////////////////////
+	// Public Interface
+	public void writeFile(String outputpath, String sequence, String description) {
+		this.writeFile(outputpath, sequence, description,false);
 	}
-	
+	public void writeFile(File out, String sequence, String description) {
+		this.writeFile(out, sequence, description,true);
+	}
+	public void writeFile(String outputpath, String sequence, String description, boolean append) {
+		File out = new File(outputpath);
+		this.writeFile(out, sequence, description,append);
+	}
+	public void writeFile(File out, String sequence, String description, boolean append) {
+		try {
+			this.print(new FileOutputStream(out,append), sequence, description);
+		} catch (FileNotFoundException e) {
+			System.err.println("An error ocurred while writing the file");
+		}
+	}
+	public void writeFile(File out, List<Pair<String,String>> seqs,  boolean append) {
+		try {
+			this.print(new FileOutputStream(out,append), seqs);
+		} catch (FileNotFoundException e) {
+			System.err.println("An error ocurred while writing the file");
+		}
+	}
+	public void print (OutputStream out, List<Pair<String,String>> seqs) {
+		this.print(new PrintWriter(out),seqs);
+	}
+	public void print (OutputStream out, String sequence, String description ) {
+		this.print(new PrintWriter(out),sequence,description);
+	}
+	public void print (PrintWriter out,  String sequence, String description ) {
+		List<Pair<String,String>> seqs = new Vector<Pair<String,String>>();
+		seqs.add(new Pair<String, String>(description, sequence));
+		this.print(out, seqs);
+	}
+	public void print (PrintWriter out,  List<Pair<String,String>> seqs ) {
+		for (Pair<String, String> pair : seqs) {
+			out.println(">" + pair.getFirst());
+			out.println(pair.getSecond());
+		} 
+		out.close();
+	}
 }
