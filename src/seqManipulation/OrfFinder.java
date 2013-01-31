b14cae4dc2f2fe2864f145fdfe5afb662d844461
jiserte <jiserte@unq.edu.ar>
2013-01-31 12:35:11 -0300

    Removed a warning

--- src/seqManipulation/OrfFinder.java
+++ src/seqManipulation/OrfFinder.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.text.Format;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
