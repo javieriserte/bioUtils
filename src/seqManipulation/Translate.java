dfac40c1cffcfdc9d3fbf6568386e2539ac86cec
jiserte <jiserte@unq.edu.ar>
2013-02-04 11:52:19 -0300

    Removed Warnings

--- src/seqManipulation/Translate.java
+++ src/seqManipulation/Translate.java
@@ -1,11 +1,8 @@
 package seqManipulation;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
-
-import cmdGA.Parser;
 
 public class Translate {
 
