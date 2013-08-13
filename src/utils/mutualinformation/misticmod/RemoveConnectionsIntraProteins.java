cc0b73ecd8fabc6bf360fc68d80aa38e0643ce20
javier <javier@martin-FIL>
2013-08-13 17:47:00 -0300

    Eliminated UnbufferedReader

--- src/utils/mutualinformation/misticmod/RemoveConnectionsIntraProteins.java
+++ src/utils/mutualinformation/misticmod/RemoveConnectionsIntraProteins.java
@@ -1,5 +1,7 @@
 package utils.mutualinformation.misticmod;
 
+import io.bufferreaders.UncommenterBufferedReader;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,6 +9,7 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 import cmdGA.MultipleOption;
+import cmdGA.NoOption;
 import cmdGA.Parser;
 import cmdGA.SingleOption;
 import cmdGA.exceptions.IncorrectParameterTypeException;
@@ -21,62 +24,87 @@ public class RemoveConnectionsIntraProteins {
 	 */
 	public static void main(String[] args) {
 		
+		/////////////////////////////
+		// Create Command Line Parser
 		Parser parser = new Parser();
 		
+		/////////////////////////////
+		// Define Commanda Line Arguments
 		SingleOption inOpt = new SingleOption(parser, System.in, "-infile", InputStreamParameter.getParameter());
 		
 		MultipleOption prot_len_opt = new MultipleOption(parser, null, "-lengths", ',',IntegerParameter.getParameter());
 		
 		SingleOption outopt = new SingleOption(parser, System.out, "-outfile", PrintStreamParameter.getParameter());
 		
+		NoOption helpOpt = new NoOption(parser, "-help", "-h");
 		
 		try {
 			
+			/////////////////////////
+			// Parse Command Line
 			parser.parseEx(args);
 			
+			///////////////////////////////
+			// Read Command Line Arguments
+			PrintStream out = (PrintStream) outopt.getValue();
 			
+			if (helpOpt.isPresent()) {
+				
+				printHelp(out);
+				
+				System.exit(1);
+				
+			}
 			
-		} catch (IncorrectParameterTypeException e) {
+			BufferedReader in = new UncommenterBufferedReader(new InputStreamReader((InputStream) inOpt.getValue()));
 			
-			e.printStackTrace();
+			int[] lengths = getProtLengths(prot_len_opt);
 			
-		}
-	
-		PrintStream out = (PrintStream) outopt.getValue();
+			///////////////////////////////////////
+			// Remove Connections
+			removeConnections(out, in, lengths);
+			
+		} catch (IncorrectParameterTypeException | NumberFormatException | IOException e) {
+			
+			System.err.println("There was an error:" + e.getMessage());
+			
+		} 
 
-//		String nocomments = (new RemoveComments()).uncomment((InputStream) inOpt.getValue());
-		
-		BufferedReader in = new UncommenterBufferedReader(new InputStreamReader((InputStream) inOpt.getValue()));
+	}
+
+
+	public static void removeConnections(PrintStream out, BufferedReader in, 	int[] lengths) throws IOException {
 		
 		String currentLine = null;
 		
-		int[] lengths = getProtLengths(prot_len_opt);
-		
-		try {
-			while((currentLine = in.readLine())!= null) {
-				
-				String data[] = currentLine.split("\t");
-				
-				int p_1 = getProteinFromPosition(Integer.valueOf(data[0]),lengths);
+		while((currentLine = in.readLine())!= null) {
+			
+			String data[] = currentLine.split("\t");
+			
+			int p_1 = getProteinFromPosition(Integer.valueOf(data[0]),lengths);
 
-				int p_2 = getProteinFromPosition(Integer.valueOf(data[2]),lengths);
+			int p_2 = getProteinFromPosition(Integer.valueOf(data[2]),lengths);
+			
+			if (p_1!=p_2) {
 				
-				if (p_1!=p_2) {
-					
-					out.println(currentLine);
-					
-				} else {
-					
-					out.println(data[0]+"\t"+data[1]+"\t"+data[2]+"\t"+data[3]+"\t"+"-999.99");
-					
-				}
+				out.println(currentLine);
+				
+			} else {
+				
+				out.println(data[0]+"\t"+data[1]+"\t"+data[2]+"\t"+data[3]+"\t"+"-999.99");
 				
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
+			
 		}
-		
+	}
 
+
+	private static void printHelp(PrintStream out) {
+		
+		out.println("Remove Intraproteins Connections");
+		
+		out.println("  Usage: ProgramName.jar -infile file -outfile file -lenghts x,y,z -help");
+		
 	}
 
 
