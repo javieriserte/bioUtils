21f312d778e4b5c76890398f5b979750c8c40ed9
Javier Iserte <jiserte@unq.edu.ar>
2013-11-08 18:31:38 -0300

    Modified graphics using jfreechart library

--- src/utils/ProfilePlotter/ProfileWriter.java
+++ src/utils/ProfilePlotter/ProfileWriter.java
@@ -3,8 +3,12 @@ package utils.ProfilePlotter;
 import graphics.profile.PngWriter;
 import graphics.profile.Profiler;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.font.TextLayout;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
@@ -14,6 +18,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import cmdGA.Parser;
 import cmdGA.SingleOption;
@@ -71,50 +86,111 @@ public class ProfileWriter {
 		
 		double min = getMin(data);
 		
-		System.err.println("# Min: "+ min);
-		System.err.println("# Max: "+ max);
+		XYSeries series = new XYSeries("Profile");
 		
-		int[] lengths = null;
+		for (int i =0 ; i<data.length ;i++) {
+			
+			series.add(i, data[i]);
+			
+		}
 		
-		Color[] colors = new Color[]{Color.red , Color.green};
+		
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		
+		dataset.addSeries(series);
+
+	
+		JFreeChart chart = ChartFactory.createXYLineChart(
+				"XY Chart",
+				// Title
+				"x-axis",
+				// x-axis Label
+				"y-axis",
+				// y-axis Label
+				dataset,
+				// Dataset
+				PlotOrientation.VERTICAL, // Plot Orientation
+				true,
+				// Show Legend
+				true,
+				// Use tooltips
+				false
+				// Configure chart to generate URLs?
+				);
+		
+		
+		
+		XYItemRenderer a = chart.getXYPlot().getRenderer();
+		
+		chart.getXYPlot().getDomainAxis().setRange(0, data.length);
+		
+//		chart.getXYPlot().setRangeAxis(0, );
+		
+		XYLineAndShapeRenderer rr = (XYLineAndShapeRenderer) a;
+		
+		
+		BufferedImage profileImage = chart.createBufferedImage( data.length, 200);
 		
 		try {
-			lengths = readLengths(lengthsFile);
-		} catch (NumberFormatException | IOException e) {
-			System.err.println("There was an error trying read lengths file: "+ e.getMessage());
-			System.exit(1);
-		}
+		(new PngWriter()).write(out,profileImage);
+	} catch (IOException e) {
+		System.err.println("There was an error trying write png file: "+ e.getMessage());
+		System.exit(1);
 		
-		BufferedImage image = new BufferedImage(data.length, (int) 220, BufferedImage.TYPE_INT_RGB);
+	}
 		
-		BufferedImage profile = (new Profiler()).draw(data, data.length, 200, max, min);
+//				try {
+//				ChartUtilities.saveChartAsJPEG(new File("C:\\chart.jpg"), chart, 500, 300);
+//				} catch (IOException e) {
+//				System.err.println("Problem occurred creating chart.");
+//				}
+
+
 		
-		Graphics2D g = (Graphics2D) image.getGraphics();
-		
-		g.drawImage(profile, 0, 0, null);
-		
-		if (lengthsOpt.isPresent()) {
-			int lastx = 0;
-			
-			for(int i=0;i<lengths.length;i++) {
-				
-				g.setColor(colors[i % 2]);
-				
-				g.fillRect(lastx, 200, lengths[i], 20);
-				
-				lastx = lastx + lengths[i];
-				
-			}
-			
-		}
-		
-		try {
-			(new PngWriter()).write(out,image);
-		} catch (IOException e) {
-			System.err.println("There was an error trying write png file: "+ e.getMessage());
-			System.exit(1);
-			
-		}
+//		System.err.println("# Min: "+ min);
+//		System.err.println("# Max: "+ max);
+//		
+//		int[] lengths = null;
+//		
+//		Color[] colors = new Color[]{Color.red , Color.green};
+//		
+//		try {
+//			lengths = readLengths(lengthsFile);
+//		} catch (NumberFormatException | IOException e) {
+//			System.err.println("There was an error trying read lengths file: "+ e.getMessage());
+//			System.exit(1);
+//		}
+//		
+//		BufferedImage image = new BufferedImage(data.length, (int) 220, BufferedImage.TYPE_INT_RGB);
+//		
+//		BufferedImage profile = (new Profiler()).draw(data, data.length, 200, max, min);
+//		
+//		Graphics2D g = (Graphics2D) image.getGraphics();
+//		
+//		g.drawImage(profile, 0, 0, null);
+//		
+//		if (lengthsOpt.isPresent()) {
+//			int lastx = 0;
+//			
+//			for(int i=0;i<lengths.length;i++) {
+//				
+//				g.setColor(colors[i % 2]);
+//				
+//				g.fillRect(lastx, 200, lengths[i], 20);
+//				
+//				lastx = lastx + lengths[i];
+//				
+//			}
+//			
+//		}
+//		
+//		try {
+//			(new PngWriter()).write(out,image);
+//		} catch (IOException e) {
+//			System.err.println("There was an error trying write png file: "+ e.getMessage());
+//			System.exit(1);
+//			
+//		}
 		
 	}
 	
@@ -199,6 +275,75 @@ public class ProfileWriter {
 		}
 		
 		return doubles;
+		
+	}
+	
+	private void drawVerticalRuler(int paddingV, int paddingW, int plotHeight,
+			int space_1, int rulerLinesVspace, int rulerNumbersVspace,
+			int lineWidth, Graphics2D g) {
+		
+		double minRuleDiv = 0.05;
+		double maxRuleDiv = 0.2;
+		double midRuleDiv = 0.1;
+		
+		g.setStroke(new BasicStroke(2));
+
+		g.drawLine( paddingW -20, 
+			        paddingV , 
+			        paddingW -20, 
+			        paddingV + plotHeight);
+		
+		g.setStroke(new BasicStroke(1));
+		
+		// Draw Minor Marks
+		for (double i=0;i <= 1 ;i = i + minRuleDiv) {
+			
+			int xPosR = paddingW-20;
+
+			int yPosR = (paddingV + (int)Math.round(plotHeight * i)); 
+			
+			g.drawLine(xPosR , yPosR, xPosR +5 , yPosR);
+		}
+		
+		// Draw Mid Marks
+		for (double i=0;i <= 1 ;i = i + midRuleDiv) {
+			
+			int xPosR = paddingW-20;
+
+			int yPosR = (paddingV + (int)Math.round(plotHeight * i));
+			
+			g.drawLine(xPosR , yPosR, xPosR +10 , yPosR);
+		}
+
+		// Draw Major Marks
+		g.setStroke(new BasicStroke(2));
+		for (double i=0;i <= 1 ;i = i + maxRuleDiv) {
+			
+			int xPosR = paddingW-20;
+
+			int yPosR = (paddingV +(int)Math.round(plotHeight * i));
+			
+			g.drawLine(xPosR , yPosR, xPosR +15 , yPosR);
+		}
+		
+	
+		g.setFont(new Font("Verdana", Font.BOLD, 18));
+		
+		for (double i=0;i<=1;i=i+maxRuleDiv) {
+
+			int xPosR = paddingW - 70;
+		
+			String numberString = String.format(Locale.US,"%.1f", i);
+						
+			TextLayout a = new TextLayout(numberString,g.getFont(), g.getFontRenderContext());
+			
+			Rectangle2D bounds = (Rectangle2D) a.getBounds().clone();
+			
+			int yPosR = (int) (paddingV + plotHeight * (1-i) + bounds.getHeight() /2 );
+			
+			g.drawString( numberString, xPosR , yPosR);
+			
+		}
 		
 	}
 
