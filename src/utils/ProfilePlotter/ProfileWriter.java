31cd1bfe7379aabfd103fdb3a8387ed92473d013
Javier Iserte <jiserte@unq.edu.ar>
2013-11-11 14:37:43 -0300

    Modifed ProfileWriter class. Now, uses jfreechart library to create plots.

--- src/utils/ProfilePlotter/ProfileWriter.java
+++ src/utils/ProfilePlotter/ProfileWriter.java
@@ -1,42 +1,52 @@
 package utils.ProfilePlotter;
 
 import graphics.profile.PngWriter;
-import graphics.profile.Profiler;
 
-import java.awt.BasicStroke;
+import io.onelinelister.OneLineListReader;
+
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.font.TextLayout;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.IntervalMarker;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
-import org.jfree.chart.renderer.xy.XYItemRenderer;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.Layer;
+import org.jfree.ui.RectangleAnchor;
+import org.jfree.ui.TextAnchor;
 
 import cmdGA.Parser;
 import cmdGA.SingleOption;
 import cmdGA.exceptions.IncorrectParameterTypeException;
+import cmdGA.parameterType.FontParameter;
 import cmdGA.parameterType.InFileParameter;
 import cmdGA.parameterType.InputStreamParameter;
 import cmdGA.parameterType.OutFileParameter;
+import cmdGA.parameterType.StringParameter;
 
+/**
+ * Draws a plot, from a given set of values, as a profile.
+ * Regions can be marked on the plot, with labels. Regions are described only by 
+ * its lengths, are expected to be ordered as desired to be shown. Regions starts 
+ * from position 1 or the next position from the last region.   
+ * 
+ * Only one data series is expected. Each point consist in only one y-value, 
+ * the x.value is the reading order. 
+ * 
+ * Uses jfreechart library to create the plot.
+ * 
+ * @author Javier Iserte
+ *
+ */
 public class ProfileWriter {
 	
 	public static void main(String[] args) {
@@ -53,9 +63,16 @@ public class ProfileWriter {
 		
 		SingleOption lengthsOpt = new SingleOption(parser, null, "-lengths", InFileParameter.getParameter());
 		
+		SingleOption namesOpt = new SingleOption(parser, null, "-names", InFileParameter.getParameter());
+		
+		SingleOption titleOpt = new SingleOption(parser, "Profile", "-title", StringParameter.getParameter());
+		
+		SingleOption labelFontOpt = new SingleOption(parser, new Font("Arial", Font.BOLD, 24) , "-font", FontParameter.getParameter());
+		
+		//Font parameter accepts: "[fontname, fontstyle, size]" or "[fontname, fontstyle]" or "[fontname]".
+		
 		//////////////////////////////
 		// Try to parse command line
-		
 		try {
 			parser.parseEx(args);
 		} catch (IncorrectParameterTypeException e) {
@@ -72,21 +89,130 @@ public class ProfileWriter {
 		
 		File lengthsFile = (File) lengthsOpt.getValue();
 		
+		File namesFile = (File) namesOpt.getValue();
+		
+		String title = (String) titleOpt.getValue();
+		
+		Font plotLabelFont = (Font) labelFontOpt.getValue();
+		
 		/////////////////////////////
 		// read profile data
 		double[] data = null;
+		
 		try {
 			 data = readData(in);
 		} catch (IOException e) {
 			System.err.println("There was an error trying to read data: "+ e.getMessage());
 			System.exit(1);
 		} 
+
+		/////////////////////////////////
+		// Define plots colors and fonts
+		Color[] colors = new Color[]{new Color(255,0, 0, 50) , new Color(0, 255, 0, 50)};
+		Color plotLinesColor = Color.black;
 		
-		double max = getMax(data);
+		//////////////////////////////
+		// Create Chart and add profile
+		JFreeChart chart = createBasicChart(title, data, plotLinesColor);
+
+		/////////////////////////////////////
+		// Add region markers 
+		addMarkerRegions(lengthsFile, namesFile, colors, plotLabelFont,	chart);
 		
-		double min = getMin(data);
+		/////////////////////////////////////
+		// Export Image
+		BufferedImage profileImage = chart.createBufferedImage( data.length, 300);
 		
-		XYSeries series = new XYSeries("Profile");
+		try {
+			
+			(new PngWriter()).write(out,profileImage);
+			
+		} catch (IOException e) {
+			
+			System.err.println("There was an error trying write png file: "+ e.getMessage());
+			
+			System.exit(1);
+		
+		}
+		/////////////////////////
+		// End of Main Code
+		/////////////////////////
+	}
+
+	/////////////////////////////////////////
+	// Private Methods
+	/**
+	 * Draws region markers into a given chart. Region data must be provided in 
+	 * a file. Also, add label with each region names if a source file is provided.
+	 *  
+	 * @param lengthsFile
+	 * @param namesFile
+	 * @param colors
+	 * @param plotLabelFont
+	 * @param chart
+	 */
+	private static void addMarkerRegions(File lengthsFile,
+			File namesFile, Color[] colors, Font plotLabelFont, JFreeChart chart) {
+		
+		try {
+		
+			if (lengthsFile!=null) {
+			
+				int[] lengths = readLengths(lengthsFile);
+				
+				String[] names = readNames(namesFile);
+				
+				int begin = 0;
+
+				for (int i = 0; i< lengths.length ; i++) {
+					
+					int end = begin + lengths[i];
+					
+					IntervalMarker marker = new IntervalMarker(begin+1, end, colors [i % colors.length]);
+					
+					begin = end;
+				
+					if (namesFile!=null) {
+					
+						////////////////////////////////
+						// Set Label Properties
+						marker.setLabel(names[i]);
+						marker.setLabelFont(plotLabelFont);
+						marker.setLabelAnchor(RectangleAnchor.BOTTOM);
+						marker.setLabelTextAnchor(TextAnchor.BOTTOM_CENTER);
+					
+						/////////////////////////////////
+						// Add Protein Marker
+						chart.getXYPlot().addDomainMarker(marker,Layer.BACKGROUND);
+						
+					}
+					
+				}
+				
+			} else {
+				
+				System.err.println("A valid lengths file must be provided (-lengths option).");
+				
+				System.exit(1);
+				
+			}
+
+		} catch (NumberFormatException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Creates the basic Chart. This chart includes, profile data, main title 
+	 * and x and y labels. 
+	 * @param title
+	 * @param data
+	 * @param plotLinesColor
+	 * @return
+	 */
+	private static JFreeChart createBasicChart(String title, double[] data, Color plotLinesColor) {
+		
+		XYSeries series = new XYSeries("");
 		
 		for (int i =0 ; i<data.length ;i++) {
 			
@@ -94,121 +220,73 @@ public class ProfileWriter {
 			
 		}
 		
-		
 		XYSeriesCollection dataset = new XYSeriesCollection();
 		
 		dataset.addSeries(series);
 
-	
 		JFreeChart chart = ChartFactory.createXYLineChart(
-				"XY Chart",
-				// Title
-				"x-axis",
-				// x-axis Label
-				"y-axis",
-				// y-axis Label
-				dataset,
-				// Dataset
+				title, // Title
+				"Residue position",  // x-axis Label
+				"Conservation",      // y-axis Label
+				dataset,  			 // Dataset
 				PlotOrientation.VERTICAL, // Plot Orientation
-				true,
-				// Show Legend
-				true,
-				// Use tooltips
-				false
-				// Configure chart to generate URLs?
+				false,    			// Show Legend
+				false,   			// Use tooltips
+				false 				// Configure chart to generate URLs?
 				);
-		
-		
-		
-		XYItemRenderer a = chart.getXYPlot().getRenderer();
 		
 		chart.getXYPlot().getDomainAxis().setRange(0, data.length);
 		
-//		chart.getXYPlot().setRangeAxis(0, );
+		chart.getXYPlot().getRenderer().setSeriesPaint(0, plotLinesColor);
 		
-		XYLineAndShapeRenderer rr = (XYLineAndShapeRenderer) a;
-		
-		
-		BufferedImage profileImage = chart.createBufferedImage( data.length, 200);
-		
-		try {
-		(new PngWriter()).write(out,profileImage);
-	} catch (IOException e) {
-		System.err.println("There was an error trying write png file: "+ e.getMessage());
-		System.exit(1);
-		
-	}
-		
-//				try {
-//				ChartUtilities.saveChartAsJPEG(new File("C:\\chart.jpg"), chart, 500, 300);
-//				} catch (IOException e) {
-//				System.err.println("Problem occurred creating chart.");
-//				}
-
-
-		
-//		System.err.println("# Min: "+ min);
-//		System.err.println("# Max: "+ max);
-//		
-//		int[] lengths = null;
-//		
-//		Color[] colors = new Color[]{Color.red , Color.green};
-//		
-//		try {
-//			lengths = readLengths(lengthsFile);
-//		} catch (NumberFormatException | IOException e) {
-//			System.err.println("There was an error trying read lengths file: "+ e.getMessage());
-//			System.exit(1);
-//		}
-//		
-//		BufferedImage image = new BufferedImage(data.length, (int) 220, BufferedImage.TYPE_INT_RGB);
-//		
-//		BufferedImage profile = (new Profiler()).draw(data, data.length, 200, max, min);
-//		
-//		Graphics2D g = (Graphics2D) image.getGraphics();
-//		
-//		g.drawImage(profile, 0, 0, null);
-//		
-//		if (lengthsOpt.isPresent()) {
-//			int lastx = 0;
-//			
-//			for(int i=0;i<lengths.length;i++) {
-//				
-//				g.setColor(colors[i % 2]);
-//				
-//				g.fillRect(lastx, 200, lengths[i], 20);
-//				
-//				lastx = lastx + lengths[i];
-//				
-//			}
-//			
-//		}
-//		
-//		try {
-//			(new PngWriter()).write(out,image);
-//		} catch (IOException e) {
-//			System.err.println("There was an error trying write png file: "+ e.getMessage());
-//			System.exit(1);
-//			
-//		}
+		return chart;
 		
 	}
 	
-	
-
-	private static int[] readLengths(File lengthsFile) throws NumberFormatException, IOException {
+	/**
+	 * Read names of regions from a file is a valid one is provided.
+	 * @param namesFile
+	 * @return a list of region names read from a file
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
+	private static String[] readNames(File namesFile) throws NumberFormatException, IOException {
 		
-		String currentline = null;
+		if (namesFile==null) {
 		
-		List<Integer> results = new ArrayList<>();
-		
-		BufferedReader in = new BufferedReader(new FileReader(lengthsFile));
-		
-		while((currentline=in.readLine())!=null) {
-			
-			results.add(Integer.valueOf(currentline));
+			return null;
 			
 		}
+		
+		OneLineListReader<String> reader = OneLineListReader.createOneLineListReaderForString();
+		
+		List<String> results = reader.read(namesFile);
+		
+		String[] names = new String[results.size()];
+		
+		for (int i = 0; i< results.size(); i++) {
+			
+			names[i] = results.get(i);
+			
+		}
+
+		return names;
+		
+	}
+	
+	/**
+	 * Read region lengths
+	 * 
+	 * @param lengthsFile
+	 * @return
+	 * @throws NumberFormatException
+	 * @throws IOException
+	 */
+	private static int[] readLengths(File lengthsFile) throws NumberFormatException, IOException {
+		
+		OneLineListReader<Integer> reader = OneLineListReader.createOneLineListReaderForInteger();
+		
+		List<Integer> results = reader.read(lengthsFile);
 		
 		int[] ints = new int[results.size()];
 		
@@ -218,53 +296,21 @@ public class ProfileWriter {
 			i++;
 		}
 
-		in.close();
-
 		return ints;
 		
 	}
-
-
-
-	private static double getMin(double[] data) {
-		
-		double min = 0;
-		
-		for (double d : data) {
-			
-			min = Math.min(min,d);
-			
-		}
-		
-		return min;
-		
-	}
-
-	private static double getMax(double[] data) {
-		
-		double max = Double.NEGATIVE_INFINITY;
-		
-		for (double d : data) {
-			
-			max = Math.max(max,d);
-			
-		}
-		
-		return max;
-		
-	}
-
+	
+	/**
+	 * Read Data from a input buffer.
+	 * @param in
+	 * @return
+	 * @throws IOException
+	 */
 	private static double[] readData(BufferedReader in) throws IOException {
 		
-		String currentline = null;
+		OneLineListReader<Double> reader = OneLineListReader.createOneLineListReaderForDouble();
 		
-		List<Double> results = new ArrayList<>();
-		
-		while((currentline=in.readLine())!=null) {
-			
-			results.add(Double.valueOf(currentline));
-			
-		}
+		List<Double> results = reader.read(in);
 		
 		double[] doubles = new double[results.size()];
 		
@@ -278,73 +324,4 @@ public class ProfileWriter {
 		
 	}
 	
-	private void drawVerticalRuler(int paddingV, int paddingW, int plotHeight,
-			int space_1, int rulerLinesVspace, int rulerNumbersVspace,
-			int lineWidth, Graphics2D g) {
-		
-		double minRuleDiv = 0.05;
-		double maxRuleDiv = 0.2;
-		double midRuleDiv = 0.1;
-		
-		g.setStroke(new BasicStroke(2));
-
-		g.drawLine( paddingW -20, 
-			        paddingV , 
-			        paddingW -20, 
-			        paddingV + plotHeight);
-		
-		g.setStroke(new BasicStroke(1));
-		
-		// Draw Minor Marks
-		for (double i=0;i <= 1 ;i = i + minRuleDiv) {
-			
-			int xPosR = paddingW-20;
-
-			int yPosR = (paddingV + (int)Math.round(plotHeight * i)); 
-			
-			g.drawLine(xPosR , yPosR, xPosR +5 , yPosR);
-		}
-		
-		// Draw Mid Marks
-		for (double i=0;i <= 1 ;i = i + midRuleDiv) {
-			
-			int xPosR = paddingW-20;
-
-			int yPosR = (paddingV + (int)Math.round(plotHeight * i));
-			
-			g.drawLine(xPosR , yPosR, xPosR +10 , yPosR);
-		}
-
-		// Draw Major Marks
-		g.setStroke(new BasicStroke(2));
-		for (double i=0;i <= 1 ;i = i + maxRuleDiv) {
-			
-			int xPosR = paddingW-20;
-
-			int yPosR = (paddingV +(int)Math.round(plotHeight * i));
-			
-			g.drawLine(xPosR , yPosR, xPosR +15 , yPosR);
-		}
-		
-	
-		g.setFont(new Font("Verdana", Font.BOLD, 18));
-		
-		for (double i=0;i<=1;i=i+maxRuleDiv) {
-
-			int xPosR = paddingW - 70;
-		
-			String numberString = String.format(Locale.US,"%.1f", i);
-						
-			TextLayout a = new TextLayout(numberString,g.getFont(), g.getFontRenderContext());
-			
-			Rectangle2D bounds = (Rectangle2D) a.getBounds().clone();
-			
-			int yPosR = (int) (paddingV + plotHeight * (1-i) + bounds.getHeight() /2 );
-			
-			g.drawString( numberString, xPosR , yPosR);
-			
-		}
-		
-	}
-
 }
