fc70de3fd62ad7c1569d662db5bce4faa1b43e7f
Javier Iserte <jiserte@unq.edu.ar>
2014-07-14 16:28:12 -0300

    Added Help to ReconstructMI

--- src/utils/mutualinformation/misticmod/reconstructmatrix/ReconstructMI.java
+++ src/utils/mutualinformation/misticmod/reconstructmatrix/ReconstructMI.java
@@ -1,23 +1,43 @@
 package utils.mutualinformation.misticmod.reconstructmatrix;
 
 import io.onelinelister.OneLineListReader;
+import io.resources.ResourceContentAsString;
 
 import java.io.File;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
-import utils.mutualinformation.misticmod.MI_Position;
-import utils.mutualinformation.misticmod.MI_PositionLineParser;
-import utils.mutualinformation.misticmod.MI_Position_MortemPrinter;
-import utils.mutualinformation.misticmod.MI_Position_Printer;
+import utils.mutualinformation.misticmod.datastructures.MI_Position;
+import utils.mutualinformation.misticmod.datastructures.MI_PositionLineParser;
+import utils.mutualinformation.misticmod.datastructures.MI_Position_MortemPrinter;
+import utils.mutualinformation.misticmod.datastructures.MI_Position_Printer;
 import cmdGA2.CommandLine;
 import cmdGA2.MultipleArgumentOption;
+import cmdGA2.NoArgumentOption;
 import cmdGA2.OptionsFactory;
 import cmdGA2.SingleArgumentOption;
 import cmdGA2.returnvalues.InfileValue;
 import cmdGA2.returnvalues.IntegerValue;
 
+/**
+ * Reads several MI_Data files and creates one that contains all the data.<br>
+ * Given a data set of N proteins (each protein is P<sub>i</sub>, 
+ * with 0 < i <= N), the program reads (N*(N+1)) files of MI data between all 
+ * proteins, including every protein with itself (each MI data file corresponds 
+ * to MI(P<sub>i</sub>,P<sub>j</sub>), with j >= i). Files must given in the 
+ * right order, for data files for MI(P<sub>i</sub>,P<sub>j</sub>):
+ * <ol>
+ * <li> index j >= index i.
+ * <li> first, order by i index.
+ * <li> then, order data sets with the same index i, with the index j. 
+ * </ol>
+ * Residue enumeration for both proteins in every data file must start with 1.
+ * The first residue of every MI pair must correspond to P<sub>i</sub>, and the 
+ * second to P<sub>j</sub>, for a data file of MI(P<sub>i</sub>,P<sub>j</sub>).
+ * 
+ * @author Javier Iserte
+ */
 public class ReconstructMI {
 
 	public static void main(String[] args) {
@@ -30,9 +50,8 @@ public class ReconstructMI {
 		////////////////////////////////////
 		// Add command line arguments
 		MultipleArgumentOption<File> infilesOpt = new MultipleArgumentOption<>(cmd, "--files", ',',new ArrayList<File>() , new InfileValue()); 
-	    SingleArgumentOption<Integer> nProtOpt = new SingleArgumentOption<Integer>(cmd, "--nprot", new IntegerValue(), 1);
-	    SingleArgumentOption<Integer> sizeOpt = new SingleArgumentOption<Integer>(cmd, "--size", new IntegerValue(), 1);
 	    MultipleArgumentOption<Integer> lenOpt = new MultipleArgumentOption<Integer>(cmd, "--lengths", ',', new ArrayList<Integer>(), new IntegerValue());
+	    NoArgumentOption                helpOpt = new NoArgumentOption(cmd, "--help");
 	    SingleArgumentOption<PrintStream> outOpt= OptionsFactory.createBasicPrintStreamArgument(cmd);
 		////////////////////////////////////
 	    
@@ -44,15 +63,17 @@ public class ReconstructMI {
 	    ////////////////////////////////////
 	    // Get Values from Command line
 	    List<File> infiles = infilesOpt.getValues();
-	    int numberOfProteins = nProtOpt.getValue();
 	    List<Integer> proteinLengths = lenOpt.getValues();
-	    int size =sizeOpt.getValue();
+	    int numberOfProteins = proteinLengths.size();
 	    PrintStream out = outOpt.getValue();
 	    ////////////////////////////////////
 	    
 	    ////////////////////////////////////
-	    // Create data structure to store de result matrix
-	    MI_Position[][] newMatrix = new MI_Position[size][size];
+	    // Check for help flag
+	    if (helpOpt.isPresent()) {
+	    	System.out.println(new ResourceContentAsString().readContents("help", ReconstructMI.class));
+	    	System.exit(0);
+	    }
 	    ////////////////////////////////////
 	    
 	    ////////////////////////////////////
@@ -89,10 +110,17 @@ public class ReconstructMI {
 	    ////////////////////////////////////
 	    
 	    ////////////////////////////////////
+	    // Create a data structure to store
+	    // the entire matrix
+	    MI_Position[][] newMatrix = new MI_Position[acumm][acumm];
+	    ////////////////////////////////////
+	    
+	    ////////////////////////////////////
+	    // Reorder the input data
 	    int fileCounter = 0;
-	    for (int i = 0; i<numberOfProteins-1;i++) {
+	    for (int i = 0; i<numberOfProteins;i++) {
 	    	
-	    	for (int j=i+1;j<numberOfProteins;j++) {
+	    	for (int j=i;j<numberOfProteins;j++) {
 	    		
 	    		File currentFile = infiles.get(fileCounter);
 	    		fileCounter++;
@@ -110,14 +138,28 @@ public class ReconstructMI {
 	    			newPos.setMean_mi(mi_Position.getMean_mi());
 	    			newPos.setSd_mi(mi_Position.getSd_mi());
 	    			
-	    			out.println(printer.print(newPos));
+	    			newMatrix[mi_Position.getPos1() + startingPoint[i]-1][mi_Position.getPos2() + startingPoint[j]-1] = newPos; 
 	    			
 				}
 	    		
 	    	}
 	    	
 	    }
+	    ////////////////////////////////////
 		
+	    
+	    ////////////////////////////////////
+	    // Print out the matrix result
+	    for (int c=0; c<acumm-1;c++) {
+	    	
+		    for (int d=c+1; d<acumm;d++) {
+		    	
+		    	out.println(printer.print(newMatrix[c][d]));
+		    	
+		    }
+	    	
+	    }
+	    ////////////////////////////////////
 	}
 
 }
