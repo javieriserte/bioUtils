883f2a412cd2a5617cef3abce7646c96095c2526
Javier Iserte <jiserte@unq.edu.ar>
2014-07-14 17:36:54 -0300

    KeepUnwanted created a new MI_Position instead of modify the given one.

--- src/utils/mutualinformation/misticmod/top/KeepUnwanted.java
+++ src/utils/mutualinformation/misticmod/top/KeepUnwanted.java
@@ -6,11 +6,16 @@ public class KeepUnwanted extends UnwantedManager {
 
 	@Override
 	public MI_Position tryToKeep(MI_Position currentPosition) {
-		currentPosition.setMean_mi(-999d);
-		currentPosition.setMi(-999d);
-		currentPosition.setRaw_mi(-999d);
-		currentPosition.setSd_mi(-999d);
-		return currentPosition;
+		MI_Position newPosition = new MI_Position(currentPosition.getPos1(),
+				currentPosition.getPos2(), 
+				currentPosition.getAa1(), 
+				currentPosition.getAa2(),
+				-999d);
+		newPosition.setMean_mi(-999d);
+		newPosition.setMi(-999d);
+		newPosition.setRaw_mi(-999d);
+		newPosition.setSd_mi(-999d);
+		return newPosition;
 	}
 
 }
